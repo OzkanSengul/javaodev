928b8b0725284fc0156746bc4b974874027533e7
OZKAN SENGUL <49585737+OzkanSengul@users.noreply.github.com>
2019-10-29 08:09:56 +0300

    Create runner.java

---
+++ runner.java
@@ -0,0 +1,30 @@
+package Odev2;
+
+public class Runner {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Decimal b = new Decimal("9999") ;
+		//Decimal b2 = new Decimal("111");
+		//Binary b3 = b.add(b2);
+
+		//Binary b = new Binary("1111") ;
+		//Binary b2 = new Binary("111");
+		//System.out.println((b.add(b2)).s);
+		Decimal b = new Decimal("275") ;
+		Decimal b2 = new Decimal("12");
+		
+		
+		
+		System.out.println((b.multiply(b2)).s);
+		
+		
+
+		
+	}
+
+}
