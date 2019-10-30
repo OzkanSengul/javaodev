46e8af8c5d3d24d33e19b11d9e80af19e582d4a5
OZKAN SENGUL <49585737+OzkanSengul@users.noreply.github.com>
2019-10-30 23:46:46 +0300

    Update decimal.java

--- decimal.java
+++ decimal.java
@@ -1,5 +1,3 @@
-package Odev2;
-
 public class Decimal {
 	
 	String s = "";
@@ -58,6 +56,25 @@ public class Decimal {
 		int length = 0;
 		int flag=0;
 	
+		for(int i=0;i <= this.s.length();i++) {
+			if (this.s.charAt(0) == '0') {
+				this.s = this.s.substring(i+1);
+			}
+			else {
+				break;
+			}			
+		}
+		
+		for(int i=0;i <= d2.s.length();i++) {
+			if (d2.s.charAt(0) == '0') {
+				d2.s = d2.s.substring(i+1);
+			}
+			else {
+				break;
+			}			
+		}
+		
+		
 		if (this.s.length() == d2.s.length() && ((this.s.charAt(0)-'0')<(d2.s.charAt(0)-'0'))) {
 			length = d2.s.length();
 			String t = this.s; 
@@ -78,12 +95,15 @@ public class Decimal {
 		String resultString ="";
 		int elde=0;
 		
+		String tmpstr = ""; 
+		tmpstr = this.s;
+		
 		for(int i=1;i <= length;i++) {
 			int s1 = 0;
 			int s2 = 0;
-			this.s=resultString;
-			if(this.s.length()-i > 0) {
-				s1=this.s.charAt(this.s.length()-i)-'0';
+			
+			if(tmpstr.length()-i >= 0) {
+				s1=tmpstr.charAt(tmpstr.length()-i)-'0';
 			}
 			if(d2.s.length()-i >= 0) {
 				s2=d2.s.charAt(d2.s.length()-i)-'0';
@@ -99,14 +119,14 @@ public class Decimal {
 				resultString=cikarma+resultString;
 			}
 			else {elde=0;
-			resultString=cikarma+resultString;
+				resultString=cikarma+resultString;
 				
 			}
 			
 			
-		
+			//tmpstr=resultString;
 			
-			}
+		}
 		
 		if (flag==1) {
 			resultString="-"+resultString;
@@ -217,57 +237,40 @@ return result;}
 
 
 
-public Decimal divide (Decimal d2) {
-	
+	public Decimal divide (Decimal d2) {
+		
 
-	int length = 0;
-	int count=0;
-	boolean kontrol=true;
-	String resultString ="";
-	
-	while ((this.s.length()>d2.s.length()) && kontrol) {
+		int length = 0;
+		int count=0;
+		boolean kontrol=true;
+		String resultString ="";
 		
-		Decimal bolunen = new Decimal(this.s);
-		Decimal bolen=new Decimal(d2.s);
-		Decimal result=new Decimal ((bolunen.subtract(bolen)).s);
-		
-		
-		this.s=result.s;
-		length=this.s.length();
-		count++;
-		if(length>d2.s.length()&& (this.s.charAt(d2.s.length())>=d2.s.charAt(0))   ) {
+		while ( (this.s.length()>=d2.s.length()) ) {
 			
-		kontrol=true;	
+			Decimal bolunen = new Decimal(this.s);
+			Decimal bolen=new Decimal(d2.s);
+			Decimal result=new Decimal ((bolunen.subtract(bolen)).s);
+			
+			if (result.s.charAt(0) == '-') {
+				break;
+			}
+			
+			this.s=result.s;
+			length=this.s.length();
+			count++;
+			
+			//if (result.s.charAt(0) == '0') {
+				
+				//break;
+			//}
 		}
-		else if(length==d2.s.length() && this.s.charAt(0)>=d2.s.charAt(0)) {
-		
-		}else {
-			kontrol=false;
+	
+		Decimal count1 =new Decimal (""+count);	
+		return count1;
 		}
-		
 	
-	
-	
+	public void Display() {
+		System.out.println(this.s);
 	}
-	
-Decimal count1 =new Decimal (resultString);	
-return count1;}
-	
-	
 }
-
-
-
-
-
-
-
-
-			
-
-
-
-
-
-
 
